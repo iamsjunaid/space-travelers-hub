beda7961752b92ae5f87d22484fa7bc960005957
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-26 19:59:12 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 import logo from '../images/planet.png';
 import '../styles/Navbar.css';
+import Rockets from './Navigation/Rockets';
+import Missions from './Navigation/Missions';
+import Dragons from './Navigation/Dragons';
+import MyProfile from './Navigation/MyProfile';
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <img src={logo} alt="logo" />
-      </li>
-      <li>title</li>
-    </ul>
-    <ul>
-      <li>link1</li>
-      <li>link2</li>
-      <li>link3</li>
-    </ul>
-  </nav>
-);
+function Navbar() {
+  const [activeLink, setActiveLink] = useState('/'); // Set initial active link to "/"
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link
+              to="/"
+              onClick={() => setActiveLink('/')}
+              className={activeLink === '/' ? 'active' : ''}
+            >
+              <img src={logo} alt="logo" />
+            </Link>
+          </li>
+          <li>title</li>
+        </ul>
+        <ul>
+          <li>
+            {' '}
+            <Link
+              to="/rockets"
+              onClick={() => setActiveLink('/rockets')}
+              className={activeLink === '/rockets' ? 'active' : ''}
+            >
+              Rockets
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/missions"
+              onClick={() => setActiveLink('/missions')}
+              className={activeLink === '/missions' ? 'active' : ''}
+            >
+              Missions
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/dragons"
+              onClick={() => setActiveLink('/dragons')}
+              className={activeLink === '/dragons' ? 'active' : ''}
+            >
+              Dragons
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/profile"
+              onClick={() => setActiveLink('/quote')}
+              className={activeLink === '/quote' ? 'active' : ''}
+            >
+              My Profile
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" />
+        <Route path="/rockets" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/dragons" element={<Dragons />} />
+        <Route path="/profile" element={<MyProfile />} />
+      </Routes>
+    </>
+  );
+}
 
 export default Navbar;
