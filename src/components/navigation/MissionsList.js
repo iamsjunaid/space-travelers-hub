a4b68a84cae714aca2b1e5954a663808b1d9b4a3
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-06-27 15:31:40 +0300

    Fetch missiom from API

--- src/components/navigation/MissionsList.js
+++ src/components/navigation/MissionsList.js
@@ -1,8 +1,43 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-undef */
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchmissions } from '../../redux/missions/missionsSlice';
 
 function MissionsList() {
+  const { missions, isLoading } = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchmissions());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (isLoading) {
+    return <h2 className="missionLoading">Loading...</h2>;
+  }
+
   return (
-    <div>Missions</div>
+    <div>
+      mission list
+      <table>
+        <thead>
+          <tr>
+            <td>mission_id</td>
+            <td>mission_name</td>
+            <td>description</td>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.mission_id}>
+              <td>{mission.mission_id}</td>
+              <td>{mission.mission_name}</td>
+              <td>{mission.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- src/components/navigation/RocketsList.js
+++ src/components/navigation/RocketsList.js
@@ -10,6 +10,7 @@ function RocketsList() {
 
   useEffect(() => {
     dispatch(fetchRockets());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (isLoading) {
