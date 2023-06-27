3ebae774eff198ff987e1679315175fb9618ad84
wolterbwambale <benplaan1@gmail.com>
2023-06-27 16:11:20 +0300

    Add dragon section

--- src/components/navigation/DragonsList.js
+++ src/components/navigation/DragonsList.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchDragons } from '../../redux/dragons/dragonsSlice';
+import Dragons from '../Dragons';
 
 function DragonsList() {
+  const { dragons, isLoading } = useSelector((state) => state.dragons);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDragons());
+  }, []);
+
+  if (isLoading) {
+    return <p className="loading">Loading...</p>;
+  }
+
+  if (!dragons || dragons.length === 0) {
+    return <p>No dragons found.</p>;
+  }
+
   return (
-    <div>Dragons</div>
+    <div>
+      <ul>
+        {dragons.map((dragon) => (
+          <Dragons
+            key={dragon.id}
+            name={dragon.name}
+            type={dragon.type}
+            image={dragon.image}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
-
 export default DragonsList;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketsReducer from './rockets/rocketsSlice';
+import dragonsReducer from './dragons/dragonsSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
+    dragons: dragonsReducer,
   },
 });
 
