e9c1487a5007071f99ca3775f6c5bba4ef5985d0
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-06-28 14:58:38 +0300

    Implement mission leaving - Actions

--- src/components/navigation/Mission.js
+++ src/components/navigation/Mission.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { reserveMission, cancelReserve } from '../../redux/missions/missionsSlice';
@@ -7,6 +7,18 @@ function Mission({
   id, name, description, reserved,
 }) {
   const dispatch = useDispatch();
+  const [isMember, setIsMember] = useState(false);
+
+  const handleJoinMission = () => {
+    dispatch(reserveMission(id));
+    setIsMember(true);
+  };
+
+  const handleLeaveMission = () => {
+    dispatch(cancelReserve(id));
+    setIsMember(false);
+  };
+
   return (
     <li className="mission">
       <div>
@@ -19,21 +31,14 @@ function Mission({
           </p>
         </div>
 
+        <span className="">{isMember ? 'Active Member' : 'Not A Member'}</span>
         {!reserved && (
-          <button
-            className="reserve-mission"
-            type="button"
-            onClick={() => dispatch(reserveMission(id))}
-          >
+          <button className="reserve-mission" type="button" onClick={handleJoinMission}>
             Join Mission
           </button>
         )}
         {reserved && (
-          <button
-            className="cancel-mission"
-            type="button"
-            onClick={() => dispatch(cancelReserve(id))}
-          >
+          <button className="cancel-mission" type="button" onClick={handleLeaveMission}>
             Leave Mission
           </button>
         )}
