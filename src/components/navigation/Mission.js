91f7f2b50d743db1559814f565ee27f5d1d3e9de
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-06-29 11:06:14 +0300

    Fix styling

--- src/components/navigation/Mission.js
+++ src/components/navigation/Mission.js
@@ -1,3 +1,84 @@
+// import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import PropTypes from 'prop-types';
+// import {
+//   reserveMission,
+//   cancelReserve,
+// } from '../../redux/missions/missionsSlice';
+// import '../../styles/Mission.css';
+
+// function Mission({
+//   id, name, description, reserved,
+// }) {
+//   const dispatch = useDispatch();
+//   const [isMember, setIsMember] = useState(false);
+
+//   const handleJoinMission = () => {
+//     dispatch(reserveMission(id));
+//     setIsMember(true);
+//   };
+
+//   const handleLeaveMission = () => {
+//     dispatch(cancelReserve(id));
+//     setIsMember(false);
+//   };
+
+//   return (
+//     <>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Name</th>
+//             <th>Description</th>
+//             <th>Status</th>
+//             <th> </th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <td>{name}</td>
+//             <td><span className="description">{description}</span></td>
+//             <td>
+//               <span className="non_member active">
+//                 {isMember ? 'Active Member' : 'Not A Member'}
+//               </span>
+//             </td>
+//             <td>
+//               {!reserved && (
+//                 <button
+//                   className="join-mission"
+//                   type="button"
+//                   onClick={handleJoinMission}
+//                 >
+//                   Join Mission
+//                 </button>
+//               )}
+//               {reserved && (
+//                 <button
+//                   className="leave-mission join-mission"
+//                   type="button"
+//                   onClick={handleLeaveMission}
+//                 >
+//                   Leave Mission
+//                 </button>
+//               )}
+//             </td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     </>
+//   );
+// }
+
+// Mission.propTypes = {
+//   id: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   description: PropTypes.string.isRequired,
+//   reserved: PropTypes.bool.isRequired,
+// };
+
+// export default Mission;
+
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -24,45 +105,35 @@ function Mission({
   };
 
   return (
-    <>
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Status</th>
-          <th> </th>
-        </tr>
-        <tr>
-          <td>{name}</td>
-          <td>{description}</td>
-          <td>
-            <span className="">
-              {isMember ? 'Active Member' : 'Not A Member'}
-            </span>
-          </td>
-          <td>
-            {!reserved && (
-              <button
-                className="reserve-mission"
-                type="button"
-                onClick={handleJoinMission}
-              >
-                Join Mission
-              </button>
-            )}
-            {reserved && (
-              <button
-                className="cancel-mission"
-                type="button"
-                onClick={handleLeaveMission}
-              >
-                Leave Mission
-              </button>
-            )}
-          </td>
-        </tr>
-      </table>
-    </>
+    <tr>
+      <td>{name}</td>
+      <td><span className="description">{description}</span></td>
+      <td>
+        <span className={`non_member ${isMember ? 'active' : ''}`}>
+          {isMember ? 'Active Member' : 'Not A Member'}
+        </span>
+      </td>
+      <td>
+        {!reserved && (
+          <button
+            className="join-mission"
+            type="button"
+            onClick={handleJoinMission}
+          >
+            Join Mission
+          </button>
+        )}
+        {reserved && (
+          <button
+            className="leave-mission join-mission"
+            type="button"
+            onClick={handleLeaveMission}
+          >
+            Leave Mission
+          </button>
+        )}
+      </td>
+    </tr>
   );
 }
 
