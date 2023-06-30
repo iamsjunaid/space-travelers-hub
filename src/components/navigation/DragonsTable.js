8bf475633b491af6ec032867b3b7ab7f9aff60f6
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-30 11:28:56 +0530

    Update DragonsTable.js

--- src/components/navigation/DragonsTable.js
+++ src/components/navigation/DragonsTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../../styles/DragonsTable.css';
+import '../../styles/RocketsTable.css';
 
 const DragonsTable = ({ dragons }) => (
   <table className="dragons-table">
