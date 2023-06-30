821eb1c5f5de3aee7ae49a0f06eb3bffb138152c
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-30 11:02:42 +0530

    Create Rocket.test.js

---
+++ src/__tests__/Rocket.test.js
@@ -0,0 +1,29 @@
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import store from '../../src/redux/store';
+import Rocket from '../components/navigation/Rocket';
+
+describe('User Interactions test', () => {
+  const rocket = [
+    {
+      id: 1,
+      rocket_name: 'Falcon 9',
+      description: 'A two-stage rocket designed and manufactured by SpaceX.',
+      image: 'falcon9.jpg',
+    },
+  ];
+
+  it('will test user interaction', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Rocket reserved={false} />
+        </Provider>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
